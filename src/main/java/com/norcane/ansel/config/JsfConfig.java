5f0cba592daa7dc41bf4da96e03bd942fe9526a4
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2022-02-15 16:38:14 +0100

    Add JSF integration

---
+++ src/main/java/com/norcane/ansel/config/JsfConfig.java
@@ -0,0 +1,28 @@
+package com.norcane.ansel.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+import jakarta.faces.webapp.FacesServlet;
+import jakarta.servlet.ServletContext;
+
+@Configuration
+public class JsfConfig {
+
+    @Bean
+    ServletRegistrationBean<FacesServlet> facesServletRegistration(ServletContext servletContext) {
+        // spring boot only works if this is set
+        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+        servletContext.setInitParameter("javax.faces.FACELETS_SKIP_COMMENTS", Boolean.TRUE.toString());
+
+        // FacesServlet registration
+        final ServletRegistrationBean<FacesServlet> bean = new ServletRegistrationBean<>();
+        bean.setServlet(new FacesServlet());
+        bean.setUrlMappings(List.of("*.xhtml"));
+        bean.setLoadOnStartup(1);
+        return bean;
+    }
+}
---
+++ src/main/java/com/norcane/ansel/ui/bean/MyBean.java
@@ -0,0 +1,21 @@
+package com.norcane.ansel.ui.bean;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class MyBean {
+
+    public MyBean() {
+        System.out.println("Created!");
+    }
+
+    public String getFrom() {
+        return this.toString();
+    }
+
+    public String getDate() {
+        return new Date().toString();
+    }
+}
