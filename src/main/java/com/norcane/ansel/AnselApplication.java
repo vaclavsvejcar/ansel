5e5c8dee0b7c3c57926cae57ea6507b9bdb7df7e
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2022-02-15 15:44:53 +0100

    Spring Boot application skeleton

---
+++ src/main/java/com/norcane/ansel/AnselApplication.java
@@ -0,0 +1,13 @@
+package com.norcane.ansel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnselApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnselApplication.class, args);
+    }
+
+}
